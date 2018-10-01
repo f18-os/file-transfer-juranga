2d7ede593469c7d98bc21afcd3dd350ddb67fc13
jerry <urangagerardo@gmail.com>
2018-10-01 16:39:44 -0600

    finished lab and readME

---
+++ file-transfer-lab/README.md
@@ -0,0 +1,43 @@
+# TCP Lab
+
+***
+
+### Description
+
+This directory contains the following files: 
+
+
+* fileClient.py - a python3 executable file that serves as a basic shell that
+  can do the following:
+
+  1. Connect to the server created by running fileServer.py 
+
+  2. By inputting a file name that exists in the directory which this file exists in, you can send a file to the server.
+
+* fileServer.py - a python3 executable file that creates a naive server that listens to port 5016. 
+
+  1. Creates a server that clients can connect to in order to send files. 
+
+  2. Naive server, but can handle multiple connections.
+
+* tst.txt - a txt file used for the demo.
+
+***
+
+### Running Program
+
+1) You must be using a UNIX OS to assure that this code runs
+correctly. Running on Windows may cause issues with the piping code.
+
+2) Make sure to have any version of python3 installed. Python2 is not
+supported.
+
+3) Once the above conditions are met, you will need to run the following 2 commands on separate shells:
+
+
+	1) `python3 fileServer.py`
+
+    2) `python3 fileClient.py -s localhost:50016`
+
+
+4) Once ran, on the fileClient shell, you will be asked to input the name of a file you wish to send to the server. 
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -24,42 +24,39 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((listenAddr, listenPort))
 server.listen(2)
 
-header = {
-    "type": "",
-    "url": "",
-    "data": ""
-}
 output_file = None
 while True:
     conn, addr = server.accept()
-    x = 1
-    if not x == 2:
+    if not os.fork():
+        header = {
+            "type": "",
+            "url": "",
+            "data": ""
+        }
         while True:
-            while True:
-                data = conn.recv(100).decode()
-                if data == "":
-                    continue
-                if data[-3:] == "EOF":
-                    output_file.write(data[0:-3])
-                    conn.send("File read. Closing connection. EOF".encode())
-                    output_file.close()
+            data = conn.recv(100).decode()
+            if data == "":
+                continue
+            if data[-3:] == "EOF":
+                output_file.write(data[0:-3])
+                conn.send("File read. Closing connection. EOF".encode())
+                output_file.close()
+                conn.close()
+                sys.exit(0)
+            if data.startswith('PUT'):
+                dc = data
+                data = data.split()
+                header['type'] = data[0]
+                header['url'] = current_dir + data[1]
+                if not os.path.exists(header['url']):
+                    open(header['url'], 'w+').close()
+                else:
+                    conn.send("Closing connection. File exists in server. EOF".encode())
                     conn.close()
                     sys.exit(0)
-                if data.startswith('PUT'):
-                    dc = data
-                    data = data.split()
-                    header['type'] = data[0]
-                    header['url'] = current_dir + data[1]
-                    if not os.path.exists(header['url']):
-                        open(header['url'], 'w+').close()
-                    else:
-                        conn.send("Closing connection. File exists in server. EOF".encode())
-                        conn.close()
-                        sys.exit(0)
-                    output_file = open(header['url'], 'a')
-                    conn.send(dc.encode())
-                else:
-                    output_file.write(data)
-                    print("Should be sending data: ", data)
-                    conn.send(data.encode())
+                output_file = open(header['url'], 'a')
+                conn.send(dc.encode())
+            else:
+                output_file.write(data)
+                conn.send(data.encode())
 server.close()
